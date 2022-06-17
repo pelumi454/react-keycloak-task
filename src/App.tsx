20265734e4c08abe76e7e036e4f2f3c554efe823
pelumi454 <amoochristianah454@gmail.com>
2022-06-17 05:56:54 +0100

    add auth context to logout button

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import LoadingSpinner from './components/LoadingSpinner';
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContextProvider";
+import LoadingSpinner from "./components/LoadingSpinner";
+import Home from "./components/Home";
 
 function App() {
-  return (
-    <div className="App">
-     <LoadingSpinner/>
-    </div>
-  );
+  const authContext = useContext(AuthContext);
+
+  // Show the loading spinner while the user is not authenticated
+  if (!authContext.isAuthenticated) {
+    return <LoadingSpinner />;
+  }
+  // If the user is authenticated display the home component
+  else {
+    return <Home />;
+  }
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import AuthContextProvider from "./context/AuthContextProvider";
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <AuthContextProvider>
       <App />
     </AuthContextProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
