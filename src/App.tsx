b5295b2475c2be76051edbaae1fd8b368c5eee07
pelumi454 <amoochristianah454@gmail.com>
2022-06-17 10:00:57 +0100

    fix: app clean up

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,9 @@ import Home from "./components/Home";
 
 function App() {
   const authContext = useContext(AuthContext);
-
-  // Show the loading spinner while the user is not authenticated
   if (!authContext.isAuthenticated) {
     return <LoadingSpinner />;
   }
-  // If the user is authenticated display the home component
   else {
     return <Home />;
   }
